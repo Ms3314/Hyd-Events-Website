99106644ad63d62c1b9570d7f37d0fd85e96cda4
Samiuddin <mdsamiuddin2005@outlook.com>
2024-10-12 22:01:59 +0530

    starter files added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-3xl font-bold underline ml-10 mt-10'></p>
+    </>
+  )
+}
+
+export default App
