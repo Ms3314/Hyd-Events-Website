8e328fcebaddc5bc66eb61c8ccb07f47a28f8e94
iqrfatima <iqrafatima0623@gmail.com>
2024-10-31 21:53:17 +0530

    Carousel updated

--- Frontend/src/Components/CustomCarousel/CustomCarousel.jsx
+++ Frontend/src/Components/CustomCarousel/CustomCarousel.jsx
@@ -1,25 +1,70 @@
-import React from 'react'
-import { CCarousel, CCarouselItem, CCarouselCaption, CImage } from '@coreui/react';
-import '@coreui/coreui/dist/css/coreui.min.css';
+import React, { useState } from 'react';
+import img1 from '../../assets/img1.png';
+import pic2 from '../../assets/pic2.png';
 
-import hydEventsLogo from '../../assets/hydEventsLogo.png'; 
-import img from '../../assets/img.png'; 
+const slides = [
+    { src: img1, key: 'img1' },
+    { src: pic2, key: 'pic2' },
+];
 
-export const CustomCarousel = () => {
-  return (
-    <div>
-<CCarousel controls transition="crossfade">
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={img} alt="slide 1" />
-  </CCarouselItem>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={hydEventsLogo} alt="slide 2" />
-  </CCarouselItem>
-  {/* <CCarouselItem>
-    <CImage className="d-block w-100" src={AngularImg} alt="slide 3" />
-  </CCarouselItem> */}
-</CCarousel>
-    </div>
-  )
-}
-export default CustomCarousel
+const CustomCarousel = () => {
+    const [curr, setCurr] = useState(0);
+
+    const prev = () => {
+        setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
+    };
+
+    const next = () => {
+        setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+    };
+
+    return (
+        <div className="overflow-hidden relative">
+            <div className="flex h-80 w-70 items-center justify-center h-screen">
+                <div
+                    className="flex h-80 w-70 transition-transform ease-out duration-500"
+                    style={{ transform: `translateX(-${curr * 100}%)` }}
+                >
+                    {slides.map((slide) => (
+                        <div className="flex-shrink-0 w-full flex justify-center" key={slide.key}>
+                            <img src={slide.src} alt={slide.key} className="max-w-xs md:max-w-md lg:max-w-lg" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="absolute inset-0 flex items-center justify-between p-4">
+                <button
+                    onClick={prev}
+                    className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#000000">
+                        <path d="M400-80 0-480l400-400 56 57-343 343 343 343-56 57Z" />
+                    </svg>
+                </button>
+                <button
+                    onClick={next}
+                    className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#000000">
+                        <path d="m304-82-56-57 343-343-343-343 56-57 400 400L304-82Z" />
+                    </svg>
+                </button>
+            </div>
+
+            <div className="absolute bottom-10 right-0 left-0">
+                <div className="flex items-center justify-center">
+                    {slides.map((_, i) => (
+                        <button
+                            key={i}
+                            className={`transition-all w-3 h-3 rounded-full ${curr === i ? "bg-blue-500 p-2" : "bg-white bg-opacity-50"}`}
+                            onClick={() => setCurr(i)}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CustomCarousel;
