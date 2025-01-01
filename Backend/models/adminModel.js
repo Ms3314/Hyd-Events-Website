a32a431a3fbd58d8e83b77747b2117c0b1fc6f1d
Samiuddin <mdsamiuddin2005@outlook.com>
2025-01-01 13:03:50 +0530

    added some models

--- Backend/models/adminModel.js
+++ Backend/models/adminModel.js
@@ -1,8 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+export async function AddUser(name , email , college ,password ) {
+    const res = await prisma.organization.create({
+        data : {
+            name ,
+            email ,
+            password ,
+            college ,
+        }
+    })
+    console.log(res , "the data we send");
+}
 
-
+export async function FindUser (email) {
+    const res = await prisma.organization.findUnique({
+        where : {
+            email
+        }
+    })
+    console.log(res , "this is the user with the unique email")
+    return res ;
+}
+// the organisation will be an array over here 
+export async function AddEvent (title , description , event_date , price , registration_link , orgnisation , event_image ) {
+    const res = await prisma.event.create({
+        data : {
+            title ,
+            description ,
+            event_date , 
+        }
+    })
+}
 // few things we have to do 
 
 // - Create user route
