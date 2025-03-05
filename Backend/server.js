01398367c6b5f3541ce7636febb1e57443b77e78
Mohammed Samiuddin <66519042+Ms3314@users.noreply.github.com>
2025-03-05 23:20:27 +0530

    Update server.js  added port for render

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/user" , userRouter);
  
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
 
